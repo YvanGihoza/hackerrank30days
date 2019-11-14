28cf39e6835381eb94147dfaaa74ca6f178fa5c7
Yvan Gihoza <44585537+YvanGihoza@users.noreply.github.com>
2019-11-14 09:05:01 -0600

    Create .cpp

---
+++ Day2/.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the solve function below.
+void solve(double meal_cost, int tip_percent, int tax_percent) {
+    //int tip = (meal_cost * tip_percent)/100.0;
+    //int tax = (tip_percent * tax_percent)/100.0;
+    //double totalCost = round(meal_cost + tip + tax);
+
+    double cost = meal_cost * (1 + (tip_percent + tax_percent) / 100.0);
+
+    double totalCost = round(cost);
+
+    cout << totalCost;
+}
+
+int main()
+{
+    double meal_cost;
+    cin >> meal_cost;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    int tip_percent;
+    cin >> tip_percent;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    int tax_percent;
+    cin >> tax_percent;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    solve(meal_cost, tip_percent, tax_percent);
+
+    return 0;
+}
